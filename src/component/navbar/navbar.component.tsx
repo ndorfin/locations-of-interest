4ee8cb381eeaf51823c946f1374b07881205fc14
Mathew Boyles <hello@mathewboyles.com>
2021-08-26 15:50:12 +1200

    Hide nav while not in use

--- src/component/navbar/navbar.component.tsx
+++ src/component/navbar/navbar.component.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import "./navbar.scss";
 
 import { Logo } from "../logo/logo.component";
-import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   const variation = window.location.search.includes("syl=true") ? "SYL" : "LOI";
@@ -14,7 +13,7 @@ export const Navbar = () => {
         <Logo variation={variation} className="nav__logo__image" />
       </div>
 
-      <ul className="nav__links">
+      {/* <ul className="nav__links">
         <li className="nav__links__item">
           <NavLink
             to="/"
@@ -22,7 +21,7 @@ export const Navbar = () => {
             className="nav__links__item__link"
             activeClassName="nav__links__item__link--active">Search Locations</NavLink>
         </li>
-      </ul>
+      </ul> */}
     </nav>
   );
 }
